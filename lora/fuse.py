3286f54823e0684d5c001972f389a36c34a62783
Kimang18 <allenkhun22@gmail.com>
2025-03-16 09:24:24 +0700

    add more description to ModelCard

--- lora/fuse.py
+++ lora/fuse.py
@@ -1,6 +1,7 @@
 # Copyright © 2023 Apple Inc.
 
 import argparse
+from textwrap import dedent
 
 import mlx.core as mx
 import utils as lora_utils
@@ -8,35 +9,59 @@ from mlx.utils import tree_flatten, tree_unflatten
 
 from model import LoRALinear
 
-def upload_to_hub(path: str, name: str, torch_name_or_path: str):
+def upload_to_hub(path: str, name: str, torch_name_or_path: str, fp16: bool):
     import os
 
     from huggingface_hub import HfApi, ModelCard, logging
 
+    #card = ModelCard()
     repo_id = f"Kimang18/{name}"
-    text = f"""
----
-library_name: mlx
----
+    #repo_id = f"mlx-community/{name}"
+    text = dedent(
+        f"""
+        ---
+        library_name: mlx
+        ---
+        
+        # {name}
+        This model was converted to MLX format from [`{torch_name_or_path}`](https://github.com/openai/whisper), then fine-tined to Khmer language using two datasets:
+        - [seanghay/khmer_mpwt_speech](https://huggingface.com/datasets/seanghay/khmer_mpwt_speech)
+        - [seanghay/km-speech-corpus](https://huggingface.com/datasets/seanghay/km-speech-corpus)
 
-# {name}
-This model was converted to MLX format from [`{torch_name_or_path}`]().
-
-## Use with mlx
-```bash
-pip install mlx-whisper
-```
-
-```python
-import mlx_whisper
-
-result = mlx_whisper.transcribe(
-    "FILE_NAME",
-    path_or_hf_repo={repo_id},
-)
-```
-"""
+        It achieves the following __word error rate__ (`wer`) on 2 popular datasets:
+        - ??? on `test` split of [google/fleurs](https://huggingface.co/datasets/google/fleurs) `km-kh`
+        - ??? on `train` split of [openslr/openslr](https://huggingface.co/datasets/openslr/openslr) `SLR42`
+        
+        __NOTE__ MLX format is usable for M-chip series of Apple.
+        
+        ## Use with mlx
+        ```bash
+        pip install mlx-whisper
+        ```
+        
+        Write a python script, `example.py`, as the following
+        ```python
+        import mlx_whisper
+        
+        result = mlx_whisper.transcribe(
+            SPEECH_FILE_NAME,
+            path_or_hf_repo="{repo_id}",
+            fp16={fp16}
+        )
+        print(result['text'])
+        ```
+        Then execute this script `example.py` to see the result.
+        
+        You can also use command line in terminal
+        ```bash
+        mlx_whisper --model {repo_id} --task transcribe SPEECH_FILE_NAME --fp16 {fp16}
+        ```
+        """
+    )
     card = ModelCard(text)
+    card.data.tags = ["mlx"] if card.data.tags is None else card.data.tags + ["mlx"]
+    card.data.base_model = torch_name_or_path
+
     card.save(os.path.join(path, "README.md"))
 
     logging.set_verbosity_info()
@@ -69,8 +94,8 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--fp16",
-        type=bool,
-        default=True,
+        type=str,
+        default="true",
         help="Load data type float 16",
     )
     parser.add_argument(
@@ -94,7 +119,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    dtype = mx.float16 if args.fp16 else mx.float32
+    dtype = mx.float16 if eval(args.fp16.title()) else mx.float32
     model, tokenizer, config = lora_utils.load(args.model, dtype=dtype)
 
     # Load adapters and get number of LoRA layers
@@ -117,4 +142,4 @@ if __name__ == "__main__":
     weights = dict(tree_flatten(model.parameters()))
     lora_utils.save_model(args.save_path, weights, tokenizer, config)
     if args.upload_name:
-        upload_to_hub(args.save_path, args.upload_name, args.original_model)
+        upload_to_hub(args.save_path, args.upload_name, args.original_model, eval(args.fp16.title()))
